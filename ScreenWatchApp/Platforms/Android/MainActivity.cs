61cccca499579aa88ba4239e13c3280e146ea2c6
Dmitriy Bohomol <bogomoldima1992@gmail.com>
2024-12-15 00:35:05 +0200

    Removed the status bar

--- ScreenWatchApp/Platforms/Android/MainActivity.cs
+++ ScreenWatchApp/Platforms/Android/MainActivity.cs
@@ -1,11 +1,25 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace ScreenWatchApp
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density, ScreenOrientation = ScreenOrientation.Landscape)]
+    [Activity(Theme = "@style/Theme.AppCompat.Light.NoActionBar", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density, ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Убираем статус-бар
+            Window.AddFlags(WindowManagerFlags.Fullscreen);
+
+            // Убираем панель навигации (если нужно)
+            Window.DecorView.SystemUiVisibility = (StatusBarVisibility)(
+                SystemUiFlags.ImmersiveSticky |
+                SystemUiFlags.HideNavigation |
+                SystemUiFlags.Fullscreen);
+        }
     }
 }
